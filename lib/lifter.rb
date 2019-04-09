1a95f004caf0e7b193044b311177f0f64259bac8
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-09 17:21:00 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,60 @@
 class Lifter
   attr_reader :name, :lift_total
-
+  @@all = []
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def memberships
+
+    memberships = Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    # Membership.all.select do |membership|
+    #   membership.lifter == self
+    # end
+
+    self.memberships.map(&:gym)
+  end
+
+  def self.average_lift_total
+    total_lift = self.all.map {|lifter| lifter.lift_total}
+    total = 0
+    total_lift.each do |lift|
+      total += lift
+    end
+    avg = (total.to_f / self.all.length)
+    avg
+  end
+
+  def self.average_lift_total
+    total = self.all.map(&:lift_total)
+    sum = total.reduce {|sum, el| sum += el}
+    avg = (sum.to_f / self.all.length)
+    return avg
+  end
+
+  def total_cost_of_lifter_gym_memberships
+    cost = self.memberships.map(&:cost)
+      cost.inject{|sum,e| sum += e }
+    # Membership.all.inject{|sum,e| sum += e }
+  end
+
+  def total
+   total_cost = self.get_memberships.map(&:cost).inject(0, &:+)
+ end
+
+ def new_sign_up(gym, cost)
+    Membership.new(cost, self, gym)
+ end
+
+ def self.all
+   @@all
+ end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,23 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+lifter1 = Lifter.new("Lifter1", 10)
+lifter2 = Lifter.new("Lifter2", 20)
+lifter3 = Lifter.new("Lifter3", 30)
+lifter4 = Lifter.new("Lifter4", 40)
+lifter5 = Lifter.new("Lifter5", 50)
+
+gym1 = Gym.new("Gym1")
+gym2 = Gym.new("Gym2")
+
+membership1_0 = Membership.new(10, lifter1, gym1)
+membership1_1 = Membership.new(10, lifter1, gym2)
+membership2 = Membership.new(20, lifter2, gym2)
+membership3 = Membership.new(30, lifter3, gym1)
+membership4 = Membership.new(40, lifter4, gym2)
+membership5 = Membership.new(50, lifter5, gym1)
+membership6 = Membership.new(60, lifter5, gym2)
+
 binding.pry
 
 puts "Gains!"
