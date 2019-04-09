e4f06d5fe4e97e017e12e1e8b1d0f7647f354f89
github username <github email address>
2019-04-09 10:31:32 -0400

    finished

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,35 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+rei = Lifter.new('Rei',200)
+chris = Lifter.new('Chris',300)
+kring = Lifter.new('Kring',50)
+guy = Lifter.new('Guy',600)
+rock = Lifter.new('Rock',1000)
+
+cko = Gym.new('CKO - Kickboxing')
+retro = Gym.new('Retro Fitness')
+la_fitness = Gym.new('LA Fitness')
+regular = Gym.new('Regular Gym')
+
+Membership.new(100,rei,cko)
+Membership.new(20,rei,retro)
+Membership.new(100,chris,cko)
+Membership.new(120,rock,la_fitness)
+Membership.new(10,guy,regular)
+Membership.new(100,kring,cko)
+Membership.new(120,chris,la_fitness)
+Membership.new(20,guy,retro)
+
+rei.memberships
+rei.gyms
+rei.total_cost_of_memberships
+
+cko.memberships
+cko.lifters
+
+rei.average_lift_total_of_all_lifters
+rei.sign_up(regular,10)
 binding.pry
 
 puts "Gains!"
