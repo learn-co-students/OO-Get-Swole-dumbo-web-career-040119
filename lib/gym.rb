fa239b33790b8dee4ce40a8d64850c41673114d6
github username <github email address>
2019-04-09 09:24:21 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,42 @@
 class Gym
   attr_reader :name
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lifters
+    memberships.map do |names|
+      names.lifter
+    end
+  end
+
+  def lifter_names
+    lifters.map do |name|
+      name.name
+    end
+  end
+
+  def combined_lift
+    lifts = lifters.map {|lift|lift.lift_total}
+    lifts.inject(0){|sum,x| sum + x }
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,8 +2,31 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+gym_1 = Gym.new('Planet Fitness')
+gym_2 = Gym.new('Retro')
+gym_3 = Gym.new('YMCA')
+gym_4 = Gym.new('Blink Fitness')
+gym_5 = Gym.new('Ralls')
+
+matt = Lifter.new("Matt",2000)
+joey = Lifter.new('Joey', 145)
+rocky = Lifter.new('Rocky', 200)
+creed = Lifter.new('Creed', 215)
+apollo = Lifter.new('Apollo', 250)
+adam = Lifter.new('Adam', 2000)
+
+Membership.new(30, joey, gym_4)
+Membership.new(40, joey,gym_5)
+Membership.new(45, rocky, gym_3)
+Membership.new(15, creed, gym_2)
+Membership.new(75, apollo, gym_1)
+Membership.new(75,apollo,gym_2)
+Membership.new(30,adam,gym_1)
 
 binding.pry
+
+# test code goes here
+
+
 
 puts "Gains!"
