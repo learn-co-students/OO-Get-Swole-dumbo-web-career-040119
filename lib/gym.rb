6e83c7478240c60478d5c443c282b67893e1ac63
cogilvy <chrisogilvy@cox.net>
2019-04-09 18:45:54 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,54 @@
 class Gym
   attr_reader :name
 
+  ALL = []
+
+
   def initialize(name)
     @name = name
+    ALL << self
+
   end
+
+  # Get a list of all gyms.
+  def self.all
+    ALL
+  end
+
+  # Get a list of all memberships at a specific gym.
+  def memberships
+    #Access all memberships => [Memberships]
+    #determine wheter or not the membership belongs to this gym (self)
+    Membership.all.select do |m|
+      m.gym == self
+    end
+  end
+
+  # Get a list of all the lifters that have a membership to a specific gym.
+  def lifters
+    #Access all memberships make array of lifters from said memberships
+    self.memberships.map do |m|
+      m.lifter
+    end
+  end
+
+  # Get a list of the names of all lifters that have a membership to that gym.
+  def lifter_names
+    #get lifters then get their names
+    self.lifters.map do |l|
+      l.name
+    end
+  end
+
+  # Get the combined lift total of every lifter has a membership to that gym
+  def total_lifter_liftable_weight
+    lifter_liftable_weight = 0
+    #get lifters
+    self.lifters.each do |l|
+      #add all of their weight
+      lifter_liftable_weight += l.lift_total
+    end
+    lifter_liftable_weight
+  end
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,54 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  # Get a list of all lifters
+  def self.all
+    @@all
+  end
+
+  # Given a gym and a membership cost, sign a specific lifter up for a new gym.
+  def sign_up(cost, gym)
+    #Create membership
+    Membership.new(cost, self, gym)
+  end
+
+  # Get a list of all the memberships that a specific lifter has
+  def memberships
+    # Iterate through all of the memberships (.all)
+    Membership.all.select do |membership|
+      # Select if the current owner of the membership(lifter) == the instance of
+      # lifter this is being called on.
+      membership.lifter == self
+    end
+  end
+
+  # Get a list of all the gyms that a specific lifter has memberships to
+  def gyms
+    #Access the Memberships.all array
+    #Determine whether or not the memberships are mine/lifters
+    my_memberships = self.memberships
+    #create array of gyms from the memberships array
+    my_memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  # Get the average lift total of all lifters
+  def avg_lift_total
+
+  end
+
+  # Get the total cost of a specific lifter's gym memberships
+  def total_cost
+
+  end
+
 end
