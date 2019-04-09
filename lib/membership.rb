b342279da6eca36c0cbc312fa699b962ed993124
github username <github email address>
2019-04-08 21:33:15 -0400

    Done

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,18 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :gym, :lifter
+  @@all = []
 
-  def initialize(cost)
+
+  def initialize(gym, lifter, cost)
     @cost = cost
+    @gym = gym
+    @lifter = lifter
+    @@all << self
   end
+
+  def self.all #get a list of all membership
+    @@all
+  end
+
+
 end
